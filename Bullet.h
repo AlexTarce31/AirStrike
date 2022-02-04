50b9d44c622905bda98a1fcc323f1c2677162097
AlexTarce31 <99029288+AlexTarce31@users.noreply.github.com>
2022-02-04 16:28:32 +0200

    Add files via upload

---
+++ Bullet.h
@@ -0,0 +1,35 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include<iostream>
+
+#include<SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+
+class Bullet
+{
+private:
+
+	sf::Sprite shape;
+
+	sf::Vector2f direction;
+	float movementSpeed;
+
+public:
+	Bullet();
+	Bullet(sf::Texture* texture, float pos_x, float pos_y, float dir_x, float dir_y, float movement_speed);
+	virtual ~Bullet();
+
+	//Accessor
+	const sf::FloatRect getBounds() const;
+
+	void update();
+	void render(sf::RenderTarget* target);
+};
+
+#endif // !BULLET_H
+
---
+++ Main.cpp
@@ -0,0 +1,15 @@
+
+#include "Game.h"
+#include <time.h>
+
+
+int main()
+{
+
+	Game game;
+
+	game.run();
+
+	//Sfarsit
+	return 0;
+}
